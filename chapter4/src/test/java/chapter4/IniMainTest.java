ab99ea59d4b3349e546c36c87f7f68fc4327704e
杨珂 <mymuyii@163.com>
2019-08-23 13:35:18 +0800

    chapter4 ini配置

---
+++ chapter4/src/test/java/chapter4/IniMainTest.java
@@ -0,0 +1,38 @@
+package chapter4;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Author: yangke
+ * @Date: 2019/8/23
+ */
+public class IniMainTest {
+
+    @Test
+    public void test() {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-config-main.ini");
+
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            subject.login(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(subject.isAuthenticated());
+    }
+}
---
+++ chapter4/src/test/java/chapter4/authenticator/MyAuthenticator.java
@@ -0,0 +1,32 @@
+package chapter4.authenticator;
+
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.realm.Realm;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Author: yangke
+ * @Date: 2019/8/23
+ */
+public class MyAuthenticator extends ModularRealmAuthenticator {
+
+    public void setBytes(byte[] bytes) {
+        System.out.println(new String(bytes));
+    }
+
+    public void setArray(int[] ints) {
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public void setSet(Set<Realm> realms) {
+        System.out.println(realms);
+    }
+
+    public void setMap(Map<Object, Object> maps) {
+        System.out.println(maps);
+        System.out.println(maps.get("1"));
+    }
+}
